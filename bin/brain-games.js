6a35f499465845b28aa0f2810a9d1fba112351cc
Полина Черепанова <lyanaraz18@gmail.com>
2024-06-01 16:15:34 +0500

    1 задание

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
-node bin/brain-games.js
 console.log('Welcome to the Brain Games!');
 
+import readlineSync from 'readline-sync';
+
+const name = readlineSync.question('Your answer: ');
+console.log(`Hello, ${name}`);
