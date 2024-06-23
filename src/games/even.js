0b6eabb670c7463b0ced81183a6830873f2e10d3
Полина Черепанова <lyanaraz18@gmail.com>
2024-06-23 18:44:11 +0500

    четное

--- src/games/even.js
+++ src/games/even.js
@@ -1,21 +1,34 @@
-const startMainGame = () => {
+import randomNumber from "../randomNums.js";
+import readlineSync from 'readline-sync';
+    
+let userName = null;
+
+export default function runEvenGame () {
     console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name?');
+    userName = readlineSync.question('May I have your name?');
     console.log(`Hello, ${userName}!`);
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    runQuestion()
 };
-
-сonsole.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-// Генерация рандомного числа
-
-const randomNumber = (min, max) => {
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-  };
-  console.log(`Question: ${randomNumber(1, 5)}`);
-
-// Проверка на чётность
-
 const isEven = (num) => num % 2 === 0;
+function runQuestion () {
+    for (let i = 0; i < 3; i += 1) {
+        const question = randomNumber();
+        console.log(`Question: ${question}`);
+        const answer = readlineSync.question('Your answer:');
 
-const answer = eadlineSync.question('Your answer:');
+        let answer1 = null;
+        if (isEven(question)) {
+            answer1 = 'yes';
+        } else {
+            answer1 = 'no';
+        };
+        if (answer === answer1) {
+            console.log('Correct!');
+        } else {
+            console.log(`${answer1} is wrong answer ;(. Correct answer was ${answer}.\nLet's try again, ${userName}!`);
+            return;
+        };
+    }
+    console.log(`Congratulations, ${userName}!`);
+}
