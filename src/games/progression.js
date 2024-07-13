d60868b1c163cc24024eeb88a278c99e844e4b3d
Полина Черепанова <lyanaraz18@gmail.com>
2024-07-13 14:09:36 +0500

    Правки

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,20 +2,23 @@ import getRandomNumber from "../randomNums.js";
 
 import greeting from '../index.js';
 
-const lengthStr = 10;
-const positionElements = getRandomNumber(1, 10);
-
-const getProgression = () => {
-  const 
+const getProgression = (startNumber, stepNumber, hiddenStep) => {
+  const progression = [];
+  for (let i = 0; i < 10; i += 1) {
+    progression.push(startNumber + i * stepNumber);
+  }
+  const correctAnswer = String(progression[hiddenStep]);
+  progression[hiddenStep] = '..';
+  return [progression.join(' '), correctAnswer];
 }
 
-
-
-
-
-
-
-
+const generateRound = () => {
+  const number = getRandomNumber();
+  const stepNumber = getRandomNumber(2, 5);
+  const hiddenStep = getRandomNumber(0, 9);
+  const [qustion, answer] = getProgression(number, stepNumber, hiddenStep);
+  return [qustion, answer];
+}
 
 export default function runProgressionGame() {
   greeting('What number is missing in the progression?', generateRound);
